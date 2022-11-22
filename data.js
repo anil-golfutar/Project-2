c53882433ce7265d8242f5cc56b78e11e7a309ca
anil-golfutar <107787492+anil-golfutar@users.noreply.github.com>
2022-11-22 18:53:37 +0545

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const tweetsData = [   
   {
       handle: `@TrollBot66756542 💎`,
-      profilePic: `images/troll.jpg`,
+      profilePic: `troll.jpg`,
       likes: 27,
       retweets: 10,
       tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
@@ -13,19 +13,19 @@ export const tweetsData = [
   },    
   {
       handle: `@Elon ✅`,
-      profilePic: `images/musk.png`,
+      profilePic: `musk.png`,
       likes: 6500,
       retweets: 234,
       tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
       replies: [
                 {
               handle: `@TomCruise ✅`,
-              profilePic: `images/tcruise.png`,
+              profilePic: `tcruise.png`,
               tweetText: `Yes! Sign me up! 😎🛩`,
           },
                 {
               handle: `@ChuckNorris ✅`,
-              profilePic: `images/chucknorris.jpeg`,
+              profilePic: `chucknorris.jpeg`,
               tweetText: `I went last year😴`,
           },
       ],
@@ -35,19 +35,19 @@ export const tweetsData = [
   },
       {
       handle: `@NoobCoder12`,
-      profilePic: `images/flower.png`,
+      profilePic: `flower.png`,
       likes: 10,
       retweets: 3,
       tweetText: `Are you a coder if you only know HTML?`,
       replies: [
           {
               handle: `@StackOverflower ☣️`,
-              profilePic: `images/overflow.png`,
+              profilePic: `overflow.png`,
               tweetText: `No. Onviosuly not. Go get a job in McDonald's.`,
           },
           {
               handle: `@YummyCoder64`,
-              profilePic: `images/love.png`,
+              profilePic: `love.png`,
               tweetText: `You are wonderful just as you are! ❤️`,
           },
       ],
